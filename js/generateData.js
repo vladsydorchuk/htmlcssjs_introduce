9c090b46afa5446fef9a0b0f0e126539c882d06a
Vlad Sydorchuk <73775955+htmlcsscourse@users.noreply.github.com>
2021-03-04 23:32:19 +0200

    Changed generated method + styles

--- js/generateData.js
+++ js/generateData.js
@@ -14,7 +14,8 @@ function getDataFromJSON() {
             return response.json();
         })
         .then(json => {
-            json.data.forEach(item => createBlock(item));
+            generateHtmlBlocks(json.data);
+            addClickHandlers();
         })
         .catch(err => {
             console.error(err);
@@ -23,65 +24,35 @@ function getDataFromJSON() {
         return 'Complete';
 }
 
-let data = document.querySelector('.data');
-function createBlock(block) {
-    let section = document.createElement('section');
-    section.classList.add('language');
-
-    let sectionTitle = document.createElement('h3');
-    sectionTitle.classList.add('language__title');
-    sectionTitle.innerText = block.title;
-
-    let grid = document.createElement('div');
-    grid.classList.add('language__grid');
-
-    block.items.forEach(item => {
-        let itemInBlock = createItemInBlock(item);
-        grid.appendChild(itemInBlock);
-    });
-
-    section.appendChild(sectionTitle);
-    section.appendChild(grid);
-    data.appendChild(section);
+let container = document.querySelector('.data');
+function generateHtmlBlocks(data) {
+    container.innerHTML = data.map(block => (
+        `<section class="language">
+            <h3 class="language__title">${block.title}</h3>
+            <div class="language__grid">
+                ${block.items.map(item => (
+                    `<div class="language__item">
+                        <div>
+                            <img class="language__item-img" src=${item.image.link} alt=${item.image.alt}/>
+                            <div class="language__item-info">
+                                <h5 class="language__item-title">${item.title}</h5>
+                                <p class="language__item-text">${item.text}</p>
+                            </div>
+                        </div>
+                        <a class="language__item-link" href="${item.button.link}" target="_blank">${item.button.caption}</a>
+                    </div>`
+                )).join('')}
+            </div>
+        </section>`
+    )).join('');
 }
 
-function createItemInBlock(data) {
-    let item = document.createElement('div');
-    item.classList.add('language__item');
-    item.addEventListener('click', () => {
-        window.open(data.button.link, "_black");
-    });
-
-    let wrapper = document.createElement('div');
-
-    let img = document.createElement('img');
-    img.src = data.image.link;
-    img.alt = data.image.alt;
-    img.classList.add('language__item-img');
-
-    let itemInfo = document.createElement('div');
-    itemInfo.classList.add('language__item-info');
-
-    let title = document.createElement('h5');
-    title.classList.add('language__item-title');
-    title.innerText = data.title;
-
-    let text = document.createElement('p');
-    text.classList.add('language__item-text');
-    text.innerText = data.text;
-
-    let link = document.createElement('a');
-    link.classList.add('language__item-link');
-    link.href = data.button.link;
-    link.innerText = data.button.caption;
-    link.target = "_black";
-
-    itemInfo.appendChild(title);
-    itemInfo.appendChild(text);
-    wrapper.appendChild(img);
-    wrapper.appendChild(itemInfo);
-    item.appendChild(wrapper);
-    item.appendChild(link);
-
-    return item;
+function addClickHandlers() {
+    var items = document.querySelectorAll('.language__item');
+    items.forEach((item) => {
+        item.addEventListener('click', function(e){
+            let parent = e.target.closest('.language__item');
+            window.open(parent.children[1].href, "_black");
+        });
+    }); 
 }
